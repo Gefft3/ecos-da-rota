e806b025aeed90d0a4397ce7cea6b05c5f825ae6
Gefft3 <gefft3@gmail.com>
2024-10-05 04:02:41 -0400

    Refactor call.sh to update k_max value and include index in output file

--- Experimentos/call.sh
+++ Experimentos/call.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #Manter o padrão 'Relevantes' e 'Irrelevantes'
-# tipo='Relevantes'
-tipo='Irrelevantes'
+tipo='Relevantes'
+# tipo='Irrelevantes'
 
 #URL dos datasets de treino e teste
 url_train='../datasets/relevantes/EIOS_train.csv'
-url_test='../datasets/irrelevantes/_GPT_test.csv'
+url_test='../datasets/relevantes/EIOS_test.csv'
+# url_test='../datasets/irrelevantes/_GPT_test.csv'
 
 #Tamanho máximo do k (número de documentos retornados)
-k_max=5
+k_max=1
 
 #Tamanho máximo de tokens por prompt
 max_prompt_tokens=50000
