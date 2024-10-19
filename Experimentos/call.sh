f04c090bfab9e95aaa3e038c31c496016a63de0a
Gefft3 <gefft3@gmail.com>
2024-10-19 14:16:33 -0400

    Refactor call.sh script to use relevant datasets for training and testing

--- Experimentos/call.sh
+++ Experimentos/call.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #Manter o padrão 'Relevantes' e 'Irrelevantes'
-# tipo='Relevantes'
-tipo='Irrelevantes'
+tipo='Relevantes'
+# tipo='Irrelevantes'
 
 #URL dos datasets de treino e teste
 url_train='../datasets/relevantes/EIOS_train.csv'
-# url_test='../datasets/relevantes/EIOS_test.csv'
-url_test='../datasets/irrelevantes/_GPT_test.csv'
+url_test='../datasets/relevantes/EIOS_test_filtrado.csv'
+#url_test='../datasets/irrelevantes/_GPT_test.csv'
 
 #Tamanho máximo do k (número de documentos retornados)
-k_max=10
+k_max=9
 
 #Tamanho máximo de tokens por prompt
 max_prompt_tokens=50000
 
 python3 script.py $url_train $url_test $k_max $tipo $max_prompt_tokens
 
-echo "Execução finalizada!"
+echo "Execução finalizada!" 
