b3981b5d72f3179f0f0737dc972c670ee0cadd87
Gefft3 <gefft3@gmail.com>
2024-11-21 10:19:02 -0400

    Refactor call.sh to update url_test path, change k_max to an array, and iterate over k values for script execution

--- Experimentos/call.sh
+++ Experimentos/call.sh
@@ -8,14 +8,21 @@ tipo='Irrelevantes'
 url_train='../datasets/relevantes/EIOS_train.csv'
 
 # url_test='../datasets/relevantes/EIOS_test_filtrado.csv'
-url_test='../datasets/irrelevantes/EIOS_test_irrelevantes.csv'
+url_test='../datasets/irrelevantes/EIOS_test_irrelevantes_filtrado.csv'
 
 #Tamanho máximo do k (número de documentos retornados)
-k_max=10
+k_max=(1 2 3 4 5 6 7 8)
 
 #Tamanho máximo de tokens por prompt
 max_prompt_tokens=128000
 
-python3 main.py $url_train $url_test $k_max $tipo $max_prompt_tokens
+
+for k in "${k_max[@]}"
+do
+  python3 main.py $url_train $url_test $k $tipo $max_prompt_tokens
+done
+
+
+# python3 main.py $url_train $url_test $k_max $tipo $max_prompt_tokens
 
 echo "Execução finalizada!" 
